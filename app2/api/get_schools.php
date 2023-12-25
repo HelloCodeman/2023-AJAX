1d6897b6f1307dad34c0360df7e0e3e17dbc31e4
HelloCodeman <955khm@gmail.com>
2023-12-25 14:21:37 +0800

    增加ajax表單呈現方式的功能

--- app2/api/get_schools.php
+++ app2/api/get_schools.php
@@ -1,8 +1,15 @@
 <?php
 include_once "db.php";
-$schools = $GraduateSchool->all();
-$options = "";
-foreach ($schools as $school) {
-    $options .= "<option value='{$school['id']}'>{$school['county']}{$school['name']}</option>";
+
+$schools=$GraduateSchool->all();
+$options="";
+if(isset($_GET['id'])){
+$user=$Student->find($_GET['id']);
+}
+foreach($schools as $school){
+
+$selected=(isset($user) && ($user['graduate_at']==$school['id']))?'selected':'';
+
+$options.="<option $selected value='{$school['id']}'>{$school['county']}{$school['name']}</option>";
 }
 echo $options;
